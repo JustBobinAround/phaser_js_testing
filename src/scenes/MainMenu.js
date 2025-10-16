779e11c06c6cfd88e8426bf9547f52c79e10b93e
Robert Junkins <roberthenryjunkins@gmail.com>
2025-10-16 17:31:37 -0400

    init commit

---
+++ src/scenes/MainMenu.js
@@ -0,0 +1,32 @@
+import { Scene } from 'phaser';
+
+export class MainMenu extends Scene {
+    constructor() {
+        super('MainMenu');
+    }
+
+    create() {
+        this.add.image(512, 384, 'background');
+
+        // this.add.image(512, 300, 'logo');
+        
+        var font = {
+            fontFamily: 'Arial Black',
+            fontSize: 38,
+            color: '#ffffff',
+            stroke: '#000000',
+            strokeThickness: 8,
+            align: 'center'
+        };
+        
+        this.add.text(512, 300, 'Where\'s the sky?', font)
+            .setOrigin(0.5);
+
+        this.add.text(512, 460, 'Main Menu', font)
+            .setOrigin(0.5);
+
+        this.input.once('pointerdown', () => {
+            this.scene.start('Game');
+        });
+    }
+}
